af56517e14e51bed4dcf5bcd714c44156d4b9520
moequan <maurice.s93@web.de>
2019-11-27 09:49:42 +0100

    Implementing adress schema.

--- models/User.js
+++ models/User.js
@@ -1,34 +1,38 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
+const {
+  Schema
+} = mongoose;
+const Adress = require("./Adress")
 
 const UserSchema = new Schema({
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    birthday: {
-        type: Date
-    },
-    userName: {
-        type: String,
-        required: true,
-        unique: true
-    }
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  birthday: {
+    type: Date
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  adress: Adress
 });
 
 
 
-module.exports = mongoose.model("User",UserSchema)
+module.exports = mongoose.model("User", UserSchema)

2475aeeb3e19058c2a99989149eb61ac8f3d20bf
moequan <maurice.s93@web.de>
2019-11-27 09:49:16 +0100

    Creating Adress model.

---
+++ models/Adress.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const {
+  Schema
+} = mongoose;
+
+const AdressSchema = new Schema({
+  street: {
+    type: String,
+    required: true;
+  },
+  city : {
+    type: String,
+    required: true
+  },
+});
+
+module.exports = AdressSchema;
